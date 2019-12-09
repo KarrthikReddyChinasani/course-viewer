7425d8675958851c2dab20fc564337a993edfe9b
Karthik Reddy <karthikreddy@Karthiks-MacBook-Air.local>
2019-12-09 17:36:03 +0530

    Added courses adding to Courses component

--- src/components/courses.js
+++ src/components/courses.js
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Courses() {
-  return <div>Courses...</div>;
+class Courses extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: [],
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let { courses, value } = this.state;
+    courses.push(value);
+    this.setState({
+      courses, 
+      value: ''
+    })
+  }
+
+  render() {
+    let { courses } = this.state;
+    return (
+      <div className="courses-wrapper">
+        <h2>Courses...</h2>
+        <ul>
+          {courses.map(item => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Course: <br></br>
+              <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Courses;
